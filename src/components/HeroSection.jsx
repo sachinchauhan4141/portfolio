217534365a3718c08ae1951ecf00e994e7ccee24
sachinchau4141 <chauhansahab85@gmail.com>
2025-03-20 18:03:10 +0530

    feat: update HeroSection component with local avatar images for team members

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,43 +10,37 @@ const people = [
     id: 1,
     name: "Sachin",
     designation: "Web Developer",
-    image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    image: "/avatars/sachin.jpeg",
   },
   {
     id: 2,
     name: "Saurabh",
     designation: "App Developer",
-    image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: "/avatars/saurabh.jpg",
   },
   {
     id: 3,
     name: "Sandeep",
     designation: "Marketing",
-    image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    image: "/avatars/sandeep.jpg",
   },
   {
     id: 4,
     name: "Mohit",
     designation: "Designer",
-    image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    image: "/avatars/mohit.jpg",
   },
   {
     id: 5,
     name: "Madhav",
     designation: "Content Writer",
-    image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    image: "/avatars/madhav.jpg",
   },
   {
     id: 6,
     name: "Jeet Chauhan",
     designation: "DevOps Engineer",
-    image:
-      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    image: "/avatars/jeet.jpg",
   },
 ];
 
